93fa6af29ff6f81c8e2814b32a1223c30ef5fdeb
Faisad29 <faisad29@gmail.com>
2026-09-15 19:46:15 +0300

    Initial wedding invitation website

--- src/config/wedding.js
+++ src/config/wedding.js
@@ -9,16 +9,12 @@ export const WEDDING_CONFIG = {
     groom: 'Al-Haj Mohammed Ali Abdul Aziz'
   },
   events: {
-    mehndiHaldi: {
-      number: '01', title: 'Mehndi & Haldi', date: '29 October 2026', time: '4:00 PM',
-      place: 'At the respective family residences', locations: ["Bride's Home - Ankola", "Groom's Home - Gangavali"]
-    },
     nikah: {
-      number: '02', title: 'Mahfil-e-Nikah', date: '31 October 2026', time: '12:15 PM',
+      number: '01', title: 'Mahfil-e-Nikah', date: '31 October 2026', time: '12:15 PM',
       place: 'Nadavara Sabha Bhavana', address: 'K.L.E. Road, Ankola, Dist. Karwar', hijriDate: '19 Jumada Al-Awwal 1448', mapsUrl: 'https://maps.app.goo.gl/tQttFRF9nJQ7Xz6t6'
     },
     walima: {
-      number: '03', title: 'Walima', date: '01 November 2026', time: '11:00 AM onwards',
+      number: '02', title: 'Walima', date: '01 November 2026', time: '11:00 AM onwards',
       place: 'Ajvi Ocean Wedding Hall & Beachside', address: 'Kodibag, Karwar', mapsUrl: 'https://maps.app.goo.gl/msLdwpCzjncXdMyc6'
     }
   },
